f43c1d079ea1ca6a81c5457f48e4187329628284
Mike Goldsmith <goldsmith.mike@gmail.com>
2017-07-13 13:14:48 +0100

    add proj with couchbase logging

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Couchbase.Core;
+using Couchbase.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+	    private readonly ILogger<ValuesController> _logger;
+	    private readonly IBucket _bucket;
+
+	    public ValuesController(ILogger<ValuesController> logger, IBucketProvider provider)
+	    {
+		    _logger = logger;
+		    _bucket = provider.GetBucket("default");
+		}
+
+		// GET api/values
+		[HttpGet]
+        public IEnumerable<string> Get()
+		{
+			var result = _bucket.Get<List<string>>("data");
+			if (result.Success)
+			{
+				return result.Value;
+			}
+
+			_logger.LogDebug("error retriving items");
+			return null;
+		}
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Couchbase.Extensions.DependencyInjection;
+using Couchbase.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+	        env.ConfigureNLog("nlog.config");
+		}
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+			// Add framework services.
+			services.AddMvc();
+	        services.AddCouchbase(Configuration.GetSection("Couchbase"));
+		}
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+	        //add NLog to ASP.NET Core
+	        loggerFactory.AddNLog();
+			LogManager.ConfigureLoggerFactory(loggerFactory);
+
+	        //add NLog.Web
+	        app.AddNLogWeb();
+
+            app.UseMvc();
+        }
+    }
+}
